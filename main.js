fd7e44cfaccf5eea5461f1c6692d631362cbd35c
Harrison Showman <harrison@showizala.com>
2025-07-04 12:53:58 -0700

    Update UI

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-<script>
-    document.querySelectorAll('.project-link').forEach(link => {
-        link.addEventListener('click', (event) => {
-            event.preventDefault();
-            const targetUrl = link.getAttribute('href');
-            document.body.style.opacity = '0';
-            setTimeout(() => {
-                window.location.href = targetUrl;
-            }, 300);
-        });
-    });
-</script>
+document.querySelectorAll('.project-link').forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const targetUrl = link.getAttribute('href');
+        document.body.style.opacity = '0';
+        setTimeout(() => {
+            window.location.href = targetUrl;
+        }, 300);
+    })
+})
